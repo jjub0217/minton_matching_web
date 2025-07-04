adc1606d25a0642e6f6c615c668a228d46a8aa85
jjub0217 <noonssub1004@gmail.com>
2025-07-04 15:42:57 +0900

    ✨ 필터섹션 컴포넌트 구현
    필터섹션 컴포넌트 와, styled component 를 사용하여 컴포넌트 구현

---
+++ src/components/FilterSection.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+// filter-select
+const CommonSelectInputStyle = `
+  flex: 1;
+  padding: 12px 16px;
+  border: 2px solid #111;
+  border-radius: 8px;
+  background-color: #222;
+  color: #f3f4f6;
+  font-size: 14px;
+  outline: none;
+  transition: border-color 0.3s ease;
+
+  &:focus {
+    border-color: #333;
+    box-shadow: 0 0 0 3px rgba(55, 65, 81, 0.1);
+  }
+  @media (min-width: 768px) {
+    min-width: 0;
+  }
+`;
+
+// filter-section
+const FilterSectionWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 40px;
+  background-color: #111;
+  padding: 20px 0;
+  border: 1px solid #111;
+`;
+
+// filter-container
+const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: 100%;
+  max-width: 600px;
+  min-width: 440px;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: center;
+  }
+  @media (max-width: 640px) {
+    flex-direction: column;
+  }
+`;
+
+// filter-select
+const Select = styled.select`
+  ${CommonSelectInputStyle}
+
+  @media (min-width: 768px) {
+    min-width: 0;
+  }
+`;
+
+const SearchInput = styled.input`
+  ${CommonSelectInputStyle}
+  @media (min-width: 768px) {
+    min-width: 0;
+  }
+`;
+
+export const FilterSection = () => {
+  const filterGrade = ["초심", "D조", "C조", "B조", "A조"];
+  const filterRadius = ["1km 이내", "3km 이내", "C조", "B조", "A조"];
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputValue = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
+
+  return (
+    <FilterSectionWrapper>
+      <FilterContainer>
+        <Select>
+          <option>급수</option>
+          {filterGrade.map((el) => (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          ))}
+        </Select>
+        <Select>
+          <option>반경</option>
+          {filterRadius.map((el) => (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          ))}
+        </Select>
+        <SearchInput
+          type="text"
+          placeholder="장소, 제목 검색"
+          value={inputValue}
+          onChange={handleInputValue}
+        />
+      </FilterContainer>
+    </FilterSectionWrapper>
+  );
+};
