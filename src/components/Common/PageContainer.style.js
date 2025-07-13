c28ee8d7235e858e3de10e1951e7a9feb100228e
jjub0217 <noonssub1004@gmail.com>
2025-07-13 13:11:36 +0900

    ♻️ 공통 컴포넌트로 리팩토링
    공통 컴포넌트로 PageContainer 로 리팩토링

---
+++ src/components/Common/PageContainer.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  margin: 0 auto;
+  background: #18181b;
+  border-radius: 16px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.18);
+  padding: ${(props) => (props.$padding ? props.$padding : "32px 0")};
+  height: ${(props) => props.height};
+`;
+
+export const PageContainerTitle2 = styled.h2`
+  margin: ${(props) => (props.$margin ? props.$margin : "")};
+`;
+export const PageContainerTitle3 = styled.h3`
+  margin: ${(props) => (props.$margin ? props.$margin : "")};
+`;
