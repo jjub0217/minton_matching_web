06428c54788aef1c7959600c6fe765dd8b2debfe
jjub0217 <noonssub1004@gmail.com>
2025-07-13 13:37:55 +0900

    ✨ 매칭 상세 페이지역에 모임 리뷰 작성 버튼 기능 추가
    모임 시간이 지난 경우에만 MatchReviewButton이 노출되도록 ParticipantsContainer에 기능 추가

--- src/pages/MatchDetail/component/Participant/ParticipantsContainer.jsx
+++ src/pages/MatchDetail/component/Participant/ParticipantsContainer.jsx
@@ -1,13 +1,34 @@
+import { useMemo } from "react";
 import { JoinButton } from "../../../../components/Button/JoinButton";
 import { LikeButton } from "../../../../components/Button/LikeButton";
-import {
-  ParticipantActions,
-  ParticipantCount,
-  Section,
-} from "../../component/Participant/Participant.styles";
+import { MatchReviewButton } from "../../../../components/Button/MatchReviewButton";
+import { ActionButtons } from "../../../../components/Common/ActionButtons.style";
 import { ParticipantMembersExpandable } from "./ParticipantMembersExpandable";
+import { ParticipantCount, Section } from "./ParticipantsContainer.styles";
 
-export const ParticipantsContainer = ({ participants }) => {
+export const ParticipantsContainer = ({ participants, time }) => {
+  const parseMeetingTime = (timeStr) => {
+    const regex = /(\d{1,2})월 (\d{1,2})일.*(오전|오후)\s*(\d{1,2}):(\d{2})/;
+    const match = timeStr.match(regex);
+
+    if (!match) return null;
+
+    const [, month, day, ampm, hourStr, minuteStr] = match;
+    let hour = parseInt(hourStr, 10);
+    const minute = parseInt(minuteStr, 10);
+
+    if (ampm === "오후" && hour !== 12) hour += 12;
+    if (ampm === "오전" && hour === 12) hour = 0;
+
+    const year = new Date().getFullYear(); // 현재 연도 사용
+    return new Date(year, month - 1, day, hour, minute);
+  };
+
+  const isPastMeeting = useMemo(() => {
+    const postedDate = parseMeetingTime(time);
+    return postedDate < new Date(); // 과거 시점인지
+  }, [time]);
+
   return (
     <Section>
       {/* 미팅 참여인원 */}
@@ -31,10 +52,12 @@ export const ParticipantsContainer = ({ participants }) => {
       <ParticipantMembersExpandable participants={participants.list} />
 
       {/* 미팅 참여 여부 영역 */}
-      <ParticipantActions>
+      <ActionButtons $mt={"28px"}>
         <LikeButton />
         <JoinButton />
-      </ParticipantActions>
+        {isPastMeeting && <MatchReviewButton />}
+        {/* <MatchReviewButton /> */}
+      </ActionButtons>
 
       {/* 다른 모임 추천 영역 */}
     </Section>
