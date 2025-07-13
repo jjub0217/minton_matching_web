43d263b4771b7034b397b62710a4a4623af08fc0
jjub0217 <noonssub1004@gmail.com>
2025-07-13 13:14:40 +0900

    ✨ 참여자 정보 컴포넌트 내의 자식 컴포넌트 구현
    참여자 정보 페이지에서 리뷰점수와 패널티 정보를 보여주는 컴포넌트 구현

---
+++ src/pages/UserProfile/components/StatBox.jsx
@@ -0,0 +1,17 @@
+import { State } from "../UserProfilePage.styles";
+
+export const StatBox = ({ label, value }) => {
+  return (
+    <State>
+      <span className="stat-label">{label}</span>
+      {label === "페널티" ? (
+        <span className="stat-value">
+          <span>{value}</span>
+          <span>회</span>
+        </span>
+      ) : (
+        <span className="stat-value">{value}</span>
+      )}
+    </State>
+  );
+};
