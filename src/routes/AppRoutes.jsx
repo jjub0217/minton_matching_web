509852e4a3742ddd413e0278152d71e20baab7fd
jjub0217 <noonssub1004@gmail.com>
2025-07-13 12:56:50 +0900

    ✨ AppRoutes 에서 라우팅 구현
    매칭생성 페이지와 장소검색 페이지, 지도페이지, 매칭생성 성공 페이지, 매칭관리페이지, 매칭리뷰페이지, 참여자리뷰페이지, 프로필 설정 페이지 라우팅 구현

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/Home/HomePage";
+import { MapPage } from "../pages/MatchCreate/MapPage";
+import { MatchCreatePage } from "../pages/MatchCreate/MatchCreatePage";
+import { MatchCreateSuccessPage } from "../pages/MatchCreate/MatchCreateSuccessPage";
+import { SearchPlacePage } from "../pages/MatchCreate/SearchPlacePage";
 import { MatchDetailPage } from "../pages/MatchDetail/MatchDetailPage";
+import { MatchManagePage } from "../pages/MatchManage/MatchManagePage";
+import { MatchReviewPage } from "../pages/MatchReview/MatchReviewPage";
+import { MyPage } from "../pages/MyPage/MyPage";
+import { ParticipantReviewPage } from "../pages/ParticipantReview/ParticipantReviewPage";
 import { UserProfilePage } from "../pages/UserProfile/UserProfilePage";
 
 export const AppRoutes = () => {
@@ -9,6 +17,17 @@ export const AppRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/detail/:id" element={<MatchDetailPage />} />
       <Route path="/userInfo/:userId" element={<UserProfilePage />} />
+      <Route path="/match/create" element={<MatchCreatePage />} />
+      <Route path="/match/create/search-place" element={<SearchPlacePage />} />
+      <Route path="/match/create/map" element={<MapPage />} />
+      <Route
+        path="/match/create/success"
+        element={<MatchCreateSuccessPage />}
+      />
+      <Route path="/match/manage" element={<MatchManagePage />} />
+      <Route path="/:id/review" element={<MatchReviewPage />} />
+      <Route path="/:id/participant" element={<ParticipantReviewPage />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 };
