64c2a1956c61f581e86499713062c8cb719ad60b
jjub0217 <noonssub1004@gmail.com>
2025-07-14 23:33:48 +0900

    ♿️ 추천 아이템 카드 접근성 및 시맨틱 마크업 개선
    - RecommendCard에 role=group 추가

--- src/pages/MatchDetail/component/Recommend/RecommendationItem.jsx
+++ src/pages/MatchDetail/component/Recommend/RecommendationItem.jsx
@@ -6,14 +6,14 @@ import {
 
 export const RecommendationItem = ({ title, image, location }, ref) => {
   return (
-    <RecommendCard ref={ref}>
+    <RecommendCard role="group">
       <RecommendCardImage>
-        <img src={image} alt={title} />
+        <img src={image} alt={`${title} 대표 이미지`} />
       </RecommendCardImage>
 
       <RecommendCardInfo>
         <h3 className="recommend-title-text">{title}</h3>
-        <div className="recommend-town">{location}</div>
+        <p className="recommend-town">{location}</p>
       </RecommendCardInfo>
     </RecommendCard>
   );
