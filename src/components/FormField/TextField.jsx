a6c03681a0aa17322b9740d3bd657cef0373d730
jjub0217 <noonssub1004@gmail.com>
2025-07-16 16:12:54 +0900

    ✨ TextField 텍스트 입력 컴포넌트 생성
    - 페이지 종류에 따라 상태 훅(useMatchField, useReviewField, useUserField) 자동 선택
    - FormField를 기반으로 만든 상태 연결형 텍스트 필드 컴포넌트
    - matchCreate, review, mypage 등 여러 페이지에서 재사용 가능

---
+++ src/components/FormField/TextField.jsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+import { useMatchField } from "../../hooks/useMatchField";
+import { useReviewField } from "../../hooks/useREviewField";
+import { useUserField } from "../../hooks/useUserField";
+import { FormField } from "./FormField";
+
+export const TextField = memo(({ label, placeholder, id, type, page }) => {
+  console.log(`${label} 렌더링`);
+  const { value, onChange } =
+    page === "matchCreate"
+      ? useMatchField(id)
+      : page === "review"
+      ? useReviewField(id)
+      : useUserField(id);
+
+  return (
+    <FormField
+      label={label}
+      id={id}
+      type={type}
+      name={id}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      page={page}
+    />
+  );
+});
