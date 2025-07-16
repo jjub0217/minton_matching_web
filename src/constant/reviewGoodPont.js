b5fbdf95e0e818e457ef5b3ec21e4e571badb94c
jjub0217 <noonssub1004@gmail.com>
2025-07-17 00:02:51 +0900

    📦 리뷰 좋은점 항목을 정의한 REVIEW_GOOD_POINTS 상수 추가
    - GoodPointSelector에서 사용할 리뷰 칭찬 문구 리스트 정의
    - id와 value를 가진 객체 배열 형태로 구성
    - 사용자에게 선택지를 명확히 제공할 수 있도록 준비

---
+++ src/constant/reviewGoodPont.js
@@ -0,0 +1,7 @@
+export const REVIEW_GOOD_POINTS = [
+  { id: 2, value: "친절해요" },
+  { id: 21, value: "매너가 좋아요" },
+  { id: 22, value: "열정적이에요" },
+  { id: 23, value: "피드백이 좋아요" },
+  { id: 24, value: "시간 약속을 잘 지켜요" },
+];
