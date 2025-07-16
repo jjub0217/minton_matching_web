54b9a6c98eb4cd21d857e0db4e102c1343569e78
jjub0217 <noonssub1004@gmail.com>
2025-07-16 19:08:02 +0900

    ✨ 승인 버튼 컴포넌트 ApprovalButton 생성
    - 재사용 가능한 승인을 위한 버튼 UI 컴포넌트 분리
    - ApproveButton 스타일 적용 및 접근성 고려 (type, title, span 구조)

---
+++ src/components/Button/ApprovalButton.jsx
@@ -0,0 +1,9 @@
+import { ApproveButton } from "../Common/Button.styles";
+
+export const ApprovalButton = () => {
+  return (
+    <ApproveButton type="button" title="승인">
+      <span>승인</span>
+    </ApproveButton>
+  );
+};
