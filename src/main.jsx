295e1ed3ff24d7721ce797edf6baec4cc082462d
jjub0217 <noonssub1004@gmail.com>
2025-07-16 23:54:37 +0900

    🔧 Redux Provider 설정 및 전역 상태 관리 적용
    - index.js에 Provider를 추가하여 Redux 전역 상태 관리 설정
    - store 설정 파일을 import하여 App에 주입
    - 전체 컴포넌트에서 Redux 상태 접근 가능하도록 구조 개선

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { store } from "./store/store";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
