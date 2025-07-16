e0e032c919386f742eba61a1ce4f07f87adce524
jjub0217 <noonssub1004@gmail.com>
2025-07-16 15:25:17 +0900

    ✨ NotificationPage 컴포넌트 추가
    - NotificationPage 컴포넌트 생성
    - 불필요한 HomePage 제거
    - 스타일 파일 정리#

---
+++ src/pages/Notification/NotificationPage.jsx
@@ -0,0 +1,25 @@
+import { ActionButton } from "../../components/Button/ActionButton";
+import { PageContainer } from "../../components/Common/PageContainer.style";
+import { notification } from "../../data/notification.json";
+import { Header } from "./NotificationPage.style";
+import { NotificationItem } from "./components/notificationItem";
+export const NotificationPage = () => {
+  return (
+    <PageContainer $padding={"0"} height={"100vh"}>
+      <Header>
+        <div className="inner">
+          <ActionButton title="뒤로 가기" iconType="backArrow" />
+          <h1>알림</h1>
+          <ActionButton title="더보기" iconType="more" />
+        </div>
+      </Header>
+
+      <main>
+        <h2 className="blind">전체 알림</h2>
+        {notification.map((el) => (
+          <NotificationItem {...el} key={el.id} />
+        ))}
+      </main>
+    </PageContainer>
+  );
+};
