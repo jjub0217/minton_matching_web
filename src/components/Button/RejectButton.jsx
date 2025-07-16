56013b761fbaf5962e0141c4adfdd589d5cf0647
jjub0217 <noonssub1004@gmail.com>
2025-07-16 19:08:39 +0900

    ✨ 거절 버튼 컴포넌트 RejectButton 생성
    - 재사용 가능한 거절용 버튼 UI 컴포넌트 생성
    - RejectedButton 스타일 적용 및 접근성 속성 (type, title) 반영

---
+++ src/components/Button/RejectButton.jsx
@@ -0,0 +1,9 @@
+import { RejectedButton } from "../Common/Button.styles";
+
+export const RejectButton = () => {
+  return (
+    <RejectedButton type="button" title="거절">
+      <span>거절</span>
+    </RejectedButton>
+  );
+};
