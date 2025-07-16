60eaa778beeb209845a05067b5112b2293dd178a
jjub0217 <noonssub1004@gmail.com>
2025-07-17 00:41:21 +0900

    server.js ec2 동작 개선

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import fetch from "node-fetch";
+
 dotenv.config();
-console.log("✅ API 키 확인:", process.env.NAVER_ID, process.env.NAVER_SECRET);
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
 
 app.get("/api/reverse-geocode", async (req, res) => {
-  // console.log(req);
-
   const { lat, lon } = req.query;
-  console.log(lat, lon);
 
   if (!lat || !lon) {
     return res.status(400).json({ error: "Missing lat/lon parameters" });
@@ -28,9 +28,7 @@ app.get("/api/reverse-geocode", async (req, res) => {
         "X-NCP-APIGW-API-KEY": process.env.NAVER_SECRET,
       },
     });
-
     const data = await response.json();
-    // console.log("🔍 네이버 응답 데이터:", data); // 추가
     res.json(data);
   } catch (err) {
     console.error("API 요청 실패:", err);
@@ -38,10 +36,9 @@ app.get("/api/reverse-geocode", async (req, res) => {
   }
 });
 
-// 장소명으로 검색하는 API 추가
 app.get("/api/search-place", async (req, res) => {
   const { keyword } = req.query;
-  console.log("✅ 요청 도착:", keyword);
+
   if (!keyword) {
     return res
       .status(400)
@@ -58,12 +55,7 @@ app.get("/api/search-place", async (req, res) => {
         "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
       },
     });
-    console.log("🔵 네이버 응답 status:", response.status); // ✅ 추가
-
     const data = await response.json();
-    console.log("🔍 네이버 응답 데이터:", data); // 추가
-    console.log("전체 검색 결과 수:", data.total);
-    console.log("받은 items 수:", data.items.length);
     res.json(data);
   } catch (error) {
     console.error("장소 검색 에러:", error);
@@ -71,6 +63,6 @@ app.get("/api/search-place", async (req, res) => {
   }
 });
 
-app.listen(5001, () => {
-  console.log("✅ Server running at http://localhost:5001");
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
